5e2cf7b026e9fee5166ce8687db5878aa07540cf
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-16 15:56:46 -0600

    Added Helper functions

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bcrypt = require('bcryptjs')
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send("It's working! It's Working!!")
 })
+
+//User Registers- post to body and Hash the password
+
+//User Logs In
+
+// User Validation
 
 module.exports = router
