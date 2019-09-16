13784c61c3116e129c2f5403bba012d4c8dd472a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-16 16:01:04 -0600

    Testing that bcrypt is working by hashing a name

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -7,6 +7,16 @@ router.get('/', (req, res) => {
     res.send("It's working! It's Working!!")
 })
 
+//Testing bcrypt by hashing name
+
+router.get('/hash', (req, res) => {
+    const name = req.query.name;
+
+    const hash = bcrypt.hashSync(name, 12)
+
+    res.send(`the hash for ${name} id ${hash}`)
+})
+
 //User Registers- post to body and Hash the password
 
 //User Logs In
